70134d747cc0ae30cfc3213ca4f6af342ed7ae14
ValentynaJasochka <125204092+ValentynaJasochka@users.noreply.github.com>
2025-06-19 17:49:44 +0000

    Deploying to gh-pages from @ ValentynaJasochka/js-library@2149d293a247589f0c9cd094a68dd74321422bea 🚀

---
+++ html/advertising.f56efa9f.js
@@ -0,0 +1,2 @@
+!function(){var t={block:document.querySelector(".advertising-block"),text:document.querySelector(".advertising-text")},e=t.text,n=5;e.textContent="Зачекайте ".concat(n," хвилин");var o=setInterval((function(){n-=1,e.textContent="Зачекайте ".concat(n," хвилин")}),1e3);setTimeout((function(){clearInterval(o),e.style.display="none"}),1e3*n)}();
+//# sourceMappingURL=advertising.f56efa9f.js.map
