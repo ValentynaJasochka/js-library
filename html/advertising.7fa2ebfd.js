db3fc74a8cfff5387959152ecf331d4c23d02f4e
ValentynaJasochka <125204092+ValentynaJasochka@users.noreply.github.com>
2025-06-21 16:55:30 +0000

    Deploying to gh-pages from @ ValentynaJasochka/js-library@32cd48913367bf218d24b7579122cb3bc0538801 🚀

--- html/advertising.7fa2ebfd.js
+++
@@ -1,2 +0,0 @@
-const t={block:document.querySelector(".advertising-block"),text:document.querySelector(".advertising-text")},{block:e,text:n}=t;let o=5;n.textContent=`Зачекайте ${o} хвилин`;const l=setInterval((()=>{o-=1,n.textContent=`Зачекайте ${o} хвилин`}),1e3);setTimeout((()=>{clearInterval(l),n.style.display="none"}),1e3*o);
-//# sourceMappingURL=advertising.7fa2ebfd.js.map
