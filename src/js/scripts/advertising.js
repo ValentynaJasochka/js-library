32cd48913367bf218d24b7579122cb3bc0538801
ValentynaJasochka <125204092+ValentynaJasochka@users.noreply.github.com>
2025-06-21 19:55:00 +0300

    clock

--- src/js/scripts/advertising.js
+++ src/js/scripts/advertising.js
@@ -1,8 +1,13 @@
 const selectors = {
   block: document.querySelector('.advertising-block'),
   text: document.querySelector('.advertising-text'),
+  dayElem: document.querySelector('.date-day'),
+  monthElem: document.querySelector('.date-month'),
+  yearElem: document.querySelector('.date-year'),
+  dateElem: document.querySelector('.date'),
+  clock: document.querySelector('.js-digital-clock'),
 };
-const { block, text } = selectors;
+const { block, text , dayElem, dateElem, monthElem,yearElem,  clock} = selectors;
 let counter = 5;
 text.textContent = `Зачекайте ${counter} хвилин`;
 const id = setInterval(() => {
@@ -13,3 +18,26 @@ setTimeout(() => {
   clearInterval(id);
   text.style.display = 'none';
 }, counter * 1000);
+
+
+
+const nameOfMonth =["Січень", "Лютий", "Березень", "Квітень", "Травень", "Червень", "Липень", "Серпень", "Вересень", "Жовтень", "Листопад", "Грудень" ];
+const arrDay=["Неділя","Понеділок","Вівторок","Середа","Четвер","П'ятниця","Субота"];
+setInterval(()=>{
+  const currentDate = new Date();
+const day = currentDate.getDay();
+const month = currentDate.getMonth(); 
+const date = currentDate.getDate();
+const year = currentDate.getFullYear();
+const hours= currentDate.getHours();
+const minutes= currentDate.getMinutes();
+const seconds= currentDate.getSeconds();
+
+dayElem.textContent =arrDay[day];
+monthElem.textContent = nameOfMonth[month+1];
+dateElem.textContent = date;
+yearElem.textContent =year;
+clock.textContent =`Поточний час ${hours}:${minutes}:${seconds}`
+}, 1000)
+
+
