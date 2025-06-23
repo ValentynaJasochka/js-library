71f7fafd3861cdbed953fb84b47068a9dfb03fbe
ValentynaJasochka <125204092+ValentynaJasochka@users.noreply.github.com>
2025-06-23 19:48:19 +0300

    winner check

--- src/js/scripts/randomizer.js
+++ src/js/scripts/randomizer.js
@@ -1,32 +1,37 @@
 const selectors = {
   btn: document.querySelector('.js-btn'),
   box: document.querySelector('.js-container'),
-  field: document.querySelector('.js-item')
+  field: document.querySelector('.js-item'),
 };
-const {btn, box, field} = selectors;
+const { btn, box, field } = selectors;
 btn.addEventListener('click', handlerClick);
 function handlerClick() {
-
-  const promises = [...box.children].map((item, idx) =>{
-    // item.textContent = '';
-   createPromise(idx).
-  than((response)=>{item.textContent =response}).
-  catch((err) => {item.textContent = err})
-  }
-  )
-};
-function createPromise(del) {
-  return new Promise((res, rej) =>{
-   setInterval(() =>{
-     const random = Math.random();
-    if(random > 0.5) {
-      res('OK')
+  const promises = [...box.children].map(() => createPromise());
+  const response = Promise.allSettled(promises).then(items => {
+    items.forEach((item, idx) => {
+      box.children[idx].textContent = '';
+      setTimeout(() => {
+        box.children[idx].textContent = item.value || item.reason;
+        if (idx === items.length - 1) {
+          const instance = basicLightbox.create(
+            `<div>${isWinner ? 'WINNER' : 'LOSER'} <\div>`
+          );
+          instance.show();
+        }
+      }, 1000 * (idx + 1));
+    });
+    const isWinner = items.every(item => item.status === 'fulfilled');
+  });
+}
+function createPromise() {
+  return new Promise((res, rej) => {
+    const random = Math.random();
+    if (random > 0.5) {
+      res('🤩');
+    } else {
+      rej('🤬');
     }
-    else {
-      rej('NO')
-    }
-   }, 1000)
-  })
+  });
 }
 
-console.log(createPromise(5).than((response)=>`OK ${response}`).catch((err) =>`NOOO ${err}`));
+// console.log(createPromise(5).then((response)=>`OK ${response}`).catch((err) =>`NOOO ${err}`));
