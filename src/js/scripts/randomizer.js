086c09494ffd2424689dfc23a03f115c7342a43b
ValentynaJasochka <125204092+ValentynaJasochka@users.noreply.github.com>
2025-06-24 20:13:48 +0300

    slots for randomiszer

--- src/js/scripts/randomizer.js
+++ src/js/scripts/randomizer.js
@@ -1,10 +1,33 @@
 const selectors = {
-  btn: document.querySelector('.js-btn'),
+  startBtn: document.querySelector('.js-btn'),
+  slotsBtn: document.querySelector('.js-slots-number'),
   box: document.querySelector('.js-container'),
   field: document.querySelector('.js-item'),
 };
-const { btn, box, field } = selectors;
-btn.addEventListener('click', handlerClick);
+const { startBtn, slotsBtn, box, field } = selectors;
+let slotNumber = 3;
+slotsBtn.addEventListener('click', handlerSlotsBtn);
+startBtn.addEventListener('click', handlerClick);
+function handlerSlotsBtn() {
+  let gameItems = parseInt(prompt('Enter the number of slots'));
+  if (isNaN(gameItems) || gameItems < 3) {
+    alert('Please enter a valid positive number more then 3.');
+  } else if (gameItems > 11) {
+    alert('You can enter les 11 slots');
+  } else {
+    console.log(boxMarkup(gameItems));
+    box.innerHTML = '';
+    box.insertAdjacentHTML('beforeend', boxMarkup(gameItems));
+  }
+}
+function boxMarkup(num) {
+  let markup = '';
+  for (let i = 0; i < num; i += 1) {
+    markup += `<div class="js-item randomizer-item"></div>`;
+  }
+  return markup;
+}
+
 function handlerClick() {
   const promises = [...box.children].map(() => createPromise());
   const response = Promise.allSettled(promises).then(items => {
@@ -14,7 +37,9 @@ function handlerClick() {
         box.children[idx].textContent = item.value || item.reason;
         if (idx === items.length - 1) {
           const instance = basicLightbox.create(
-            `<div>${isWinner ? 'WINNER' : 'LOSER'} <\div>`
+            `<div class="randomizer-modal">${
+              isWinner ? 'WINNER' : 'LOSER'
+            } <\div>`
           );
           instance.show();
         }
@@ -34,4 +59,15 @@ function createPromise() {
   });
 }
 
-// console.log(createPromise(5).then((response)=>`OK ${response}`).catch((err) =>`NOOO ${err}`));
+const quantity = 17;
+const positions = 8;
+const timerId = setTimeout(
+  (a, b) => {
+    console.log(a * b);
+  },
+  3000,
+  quantity,
+  positions
+);
+
+console.log(timerId);
