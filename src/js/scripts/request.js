e8136f170e76a01b0c5ce56bdc09f9675e71f1ef
ValentynaJasochka <125204092+ValentynaJasochka@users.noreply.github.com>
2025-06-26 18:07:59 +0300

    fetch

--- src/js/scripts/request.js
+++ src/js/scripts/request.js
@@ -1,4 +1,14 @@
 //https://pokeapi.co
 
-const API_Key= 'd6ef5733ea0c4b55952125610252606';
-const URL = 'http://api.weatherapi.com/v1'
+const API_Key = 'd6ef5733ea0c4b55952125610252606';
+const BASE_URL = 'http://api.weatherapi.com/v1';
+const EndPOINT = '/forecast.json';
+fetch(`${BASE_URL}${EndPOINT}?key=${API_Key}&q=Krakow&days=3`)
+  .then(response => {
+    if (!response.ok) {
+      throw new Error(response.statusText);
+    }
+    return response.json();
+  })
+  .then(data => console.log(data))
+  .catch(err => console.log(err));
