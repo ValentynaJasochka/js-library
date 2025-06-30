bc270a6715158a97c6a6837ca84d232e513a7c90
ValentynaJasochka <125204092+ValentynaJasochka@users.noreply.github.com>
2025-06-30 23:05:12 +0300

    weather forecast

--- src/js/scripts/request.js
+++ src/js/scripts/request.js
@@ -5,19 +5,64 @@ const BASE_URL = 'http://api.weatherapi.com/v1';
 const EndPOINT = '/forecast.json';
 const selectors = {
   form: document.querySelector('.js-weather'),
-  weatherList: document.querySelector('.js-weather-list')
+  weatherList: document.querySelector('.js-weather-list'),
 };
-const { form, weatherList} = selectors;
-fetch(`${BASE_URL}${EndPOINT}?key=${API_Key}&q=Krakow&days=3`)
-  .then(response => {
+const { form, weatherList } = selectors;
+
+form.addEventListener('submit', handlerSearch);
+
+function handlerSearch(evt) {
+  evt.preventDefault();
+  const {
+    query: { value: country },
+    days: { value: daysLong },
+  } = evt.currentTarget.elements;
+
+  console.dir(daysLong);
+  console.dir(country);
+  // weatherList.innerHTML = createMarkup(data.forecast)
+  // console.logcreateMarkup((data.forecast.forecastday));
+
+  serviceForecast(country, daysLong)
+    .then(
+      data => (weatherList.innerHTML = createMarkup(data.forecast.forecastday))
+    )
+    .catch(err => console.log(err));
+}
+function serviceForecast(city, days) {
+  const params = new URLSearchParams({
+    key: API_Key,
+    q: city,
+    days: days,
+    lang: 'uk',
+  });
+  return fetch(`${BASE_URL}${EndPOINT}?${params}`).then(response => {
     if (!response.ok) {
       throw new Error(response.statusText);
     }
     return response.json();
-  })
-  .then(data => console.log(data))
-  .catch(err => console.log(err));
-form.addEventListener('submit', handlerSearch)
-function handlerSearch(evt) {
-
+  });
 }
+function createMarkup(arr) {
+  return arr
+    .map(
+      ({
+        date,
+        day: {
+          avgtemp_c,
+          condition: { text, icon },
+        },
+      }) => {
+        return (markup = `<li>
+          <img src="${icon}" alt="${text}">
+          <h3 >Дата:${date}</h3>
+          <h3>${text}</h3>
+          <h3>Температура:${avgtemp_c}</h3>
+          
+        </li>`);
+      }
+    )
+    .join('');
+}
+
+console.log(createMarkup([]));
