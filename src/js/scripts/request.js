abc4b2b70f2f3889cd3828fe570b74def4d7b421
ValentynaJasochka <125204092+ValentynaJasochka@users.noreply.github.com>
2025-06-30 21:02:54 +0300

    form for weather

--- src/js/scripts/request.js
+++ src/js/scripts/request.js
@@ -3,6 +3,11 @@
 const API_Key = 'd6ef5733ea0c4b55952125610252606';
 const BASE_URL = 'http://api.weatherapi.com/v1';
 const EndPOINT = '/forecast.json';
+const selectors = {
+  form: document.querySelector('.js-weather'),
+  weatherList: document.querySelector('.js-weather-list')
+};
+const { form, weatherList} = selectors;
 fetch(`${BASE_URL}${EndPOINT}?key=${API_Key}&q=Krakow&days=3`)
   .then(response => {
     if (!response.ok) {
@@ -12,3 +17,7 @@ fetch(`${BASE_URL}${EndPOINT}?key=${API_Key}&q=Krakow&days=3`)
   })
   .then(data => console.log(data))
   .catch(err => console.log(err));
+form.addEventListener('submit', handlerSearch)
+function handlerSearch(evt) {
+
+}
