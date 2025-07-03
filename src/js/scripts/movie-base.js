6e9b6b693c23cad223c4c4cccd05524bb739a44a
ValentynaJasochka <125204092+ValentynaJasochka@users.noreply.github.com>
2025-07-03 15:37:20 +0300

    observer

--- src/js/scripts/movie-base.js
+++ src/js/scripts/movie-base.js
@@ -19,7 +19,6 @@
 // .then(data => console.log(data))
 // .catch(err=> console.log(err));
 
-
 ///movies
 
 const API_Key = '1458aa5a0f3768251d977a5078bcec84';
@@ -28,30 +27,69 @@ const EndPOINT = '/trending/movie/week';
 const selectors = {
   load: document.querySelector('.js-load-more'),
   movieList: document.querySelector('.js-movie-list'),
+  target: document.querySelector('.js-guard'),
 };
-const { load, movieList } = selectors;
-let page =1;
-load.addEventListener("click", onLoadMore)
-function onLoadMore() {
-  page +=1;
-  serviceMovie(page).then(data => movieList.insertAdjacentHTML('beforeend', createMarkup(data.results))).catch(err=> console.log(err))
+const { load, movieList, target } = selectors;
+const options = {
+  root: null,
+  rootMargin: '300px',
+  threshold: 0,
+};
+let page = 1;
+let observer = new IntersectionObserver(handlerPagination, options);
+function handlerPagination(entries, observer) {
+   entries.forEach(entry => {
+       if (entry.isIntersecting) {      
+      page += 1;
+      serviceMovie(page)
+        .then(data => {
+          movieList.insertAdjacentHTML('beforeend', createMarkup(data.results));
+        })
+        .catch(err => console.log(err));
+    }
+  });
 }
-function serviceMovie(page=1) {
-return fetch(`${BASE_URL}${EndPOINT}?api_key=${API_Key}&page=${page}`)
-.then((resp) =>{
-  if (!resp) {
-    throw new Error(resp.statusText)
-  }
-  return resp.json()
-})
 
-};
-serviceMovie(1).then(data => movieList.insertAdjacentHTML('beforeend', createMarkup(data.results))).catch(err=> console.log(err));
+load.addEventListener('click', onLoadMore);
+function onLoadMore() {
+  page += 1;
+  serviceMovie(page)
+    .then(data => {
+      movieList.insertAdjacentHTML('beforeend', createMarkup(data.results));
+      if (data.page >= data.total_pages) {
+        load.hidden = true;
+      }
+    })
+    .catch(err => console.log(err));
+}
+function serviceMovie(page = 1) {
+  return fetch(`${BASE_URL}${EndPOINT}?api_key=${API_Key}&page=${page}`).then(
+    resp => {
+      if (!resp) {
+        throw new Error(resp.statusText);
+      }
+      return resp.json();
+    }
+  );
+}
+serviceMovie(page)
+  .then(data => {
+    movieList.insertAdjacentHTML('beforeend', createMarkup(data.results));
+    observer.observe(target);
+    if (data.page < data.total_pages) {
+      // load.hidden = false;
+    }
+  })
+  .catch(err => console.log(err));
 function createMarkup(arr) {
-  return arr.map(({poster_path, release_date, original_title,vote_average}) =>{ return `<li class="movie-card">
+  return arr
+    .map(({ poster_path, release_date, original_title, vote_average }) => {
+      return `<li class="movie-card">
         <img src="https://image.tmdb.org/t/p/w300${poster_path}" alt="${original_title}" class="movie-img">
         <h2 class="movie-name">${original_title}</h2>
         <p>${release_date}</p>
         <p>${vote_average}</p>
-      </li>`}).join('')
+      </li>`;
+    })
+    .join('');
 }
