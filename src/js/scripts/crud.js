945b14faec59f8732e98361ba8e4c01de21f6c18
ValentynaJasochka <125204092+ValentynaJasochka@users.noreply.github.com>
2025-07-05 15:37:26 +0300

    patch /put method

--- src/js/scripts/crud.js
+++ src/js/scripts/crud.js
@@ -1,24 +1,49 @@
 //POST and Create
-const BASE_URL ='https://jsonplaceholder.typicode.com/todos/';
-const post= { 
-completed:false,
-title: "NOthing will change the facts",
-userId: 369,
-}
-const options ={
-  method:"POST",
-  body: JSON.stringify(),
-  headers:{
+const BASE_URL = 'https://jsonplaceholder.typicode.com/todos/';
+const post = {
+  completed: false,
+  title: 'NOthing will change the facts',
+  userId: 369,
+};
+const options = {
+  method: 'POST',
+  body: JSON.stringify(post),
+  headers: {
     'Content-type': 'application/json',
-  }
-}
+  },
+};
+let elem = { id: '' };
+fetch(`${BASE_URL}`, options)
+  .then(response => {
+    if (!response.ok) {
+      throw new Error(response.statusText);
+    }
+    return response.json();
+  })
+  .then(data => {
+    console.log(data);
+    elem.id = data.id;
+  })
+  .catch(err => console.log(err));
 
-
-fetch(`${BASE_URL}`,options)
-      .then(response => {
-        console.log(response);
-        if(!response.ok) {
-          throw new Error(response.statusText)
-        }
-      }).catch(err => console.log(err))
-      
+//PATCH and PUT
+console.log(elem);
+const patch = {
+  title: 'The best day in my life',
+};
+const patchOptions =   {
+  method: 'PATCH',
+  body: JSON.stringify(patch),
+  headers: {
+    'Content-type': 'application/json',
+  },
+};
+fetch(`${BASE_URL}${1}`, patchOptions)
+  .then(response => {
+    if (!response.ok) {
+      throw new Error(response.statusText);
+    }
+    return response.json();
+  })
+  .then(data => console.log(data))
+  .catch(err => console.log(err));
