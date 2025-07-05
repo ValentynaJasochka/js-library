8d516dad124758532f8485ecdf51bb584735f40a
ValentynaJasochka <125204092+ValentynaJasochka@users.noreply.github.com>
2025-07-05 11:56:12 +0000

    Deploying to gh-pages from @ ValentynaJasochka/js-library@b50110edd18a77732fe779d76dfe96cc8b4a0cfe 🚀

---
+++ html/crud.404d62a0.js
@@ -0,0 +1,2 @@
+!function(){var o={method:"POST",body:JSON.stringify(),headers:{"Content-type":"application/json"}};fetch("".concat("https://jsonplaceholder.typicode.com/todos/"),o).then((function(o){if(console.log(o),!o.ok)throw new Error(o.statusText)})).catch((function(o){return console.log(o)}))}();
+//# sourceMappingURL=crud.404d62a0.js.map
