861e5c54b8c873ee6f1d23430b856ad583dc0096
ValentynaJasochka <125204092+ValentynaJasochka@users.noreply.github.com>
2025-07-05 12:37:48 +0000

    Deploying to gh-pages from @ ValentynaJasochka/js-library@945b14faec59f8732e98361ba8e4c01de21f6c18 🚀

--- html/crud.e1f96423.js
+++
@@ -1,2 +0,0 @@
-const o={method:"POST",body:JSON.stringify(),headers:{"Content-type":"application/json"}};fetch("https://jsonplaceholder.typicode.com/todos/",o).then((o=>{if(console.log(o),!o.ok)throw new Error(o.statusText)})).catch((o=>console.log(o)));
-//# sourceMappingURL=crud.e1f96423.js.map
