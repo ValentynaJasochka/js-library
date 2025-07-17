b027d9924fa75c0da33d0d4019e2abefe2b9db8f
ValentynaJasochka <125204092+ValentynaJasochka@users.noreply.github.com>
2025-07-17 20:04:05 +0300

    countries arr form

--- src/js/scripts/vacation.js
+++ src/js/scripts/vacation.js
@@ -1,9 +1,41 @@
-async function serviceCountry () {
-  const response = await fetch('https://restcountries.com/v3.1/name/ukraine')
-  if(!response.ok) {
-    throw new Error(response.statusText)
-  }
-  const data = await response.json()
-  console.log(data);
+const selectors = {
+  form: document.querySelector('.js-search'),
+   formContainer: document.querySelector('.js-form-container'),
+  addField: document.querySelector('.js-add'),
+};
+const { form, formContainer, addField } = selectors;
+
+
+
+addField.addEventListener('click', handlerAdd)
+form.addEventListener('submit', handlerSearch)
+function handlerAdd() {
+  formContainer.insertAdjacentHTML('beforeend', ' <input type="text" name="country" />')
 }
-serviceCountry()
+function handlerSearch(evt) {
+  evt.preventDefault()
+  const formData = new FormData(evt.currentTarget)
+  const countries = formData.getAll('country').map(item =>item.trim()).filter(item =>item)
+  console.log(countries);
+  form.reset()
+}
+
+const service = {
+  countries: ['Ukraine', 'France', 'Poland'],
+  async serviceCountry() {
+    const responses = this.countries.map(async country => {
+      const resp = await fetch(
+        `https://restcountries.com/v3.1/name/${country}`
+      );
+      //  console.log(await resp.json());
+      return resp.json();
+    });
+    //  const response = await fetch(`https://restcountries.com/v3.1/name/${this.countryName}`)
+    //   if(!response.ok) {
+    //     throw new Error(response.statusText)
+    //   }
+    //   const data = await response.json()
+    //   console.log(data);
+  },
+};
+// service.serviceCountry().then(data => data.json());
